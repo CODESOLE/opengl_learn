b3fd9ae1ec2ba6857a32534d8345872909c2edc5
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2023-10-13 12:46:25 +0300

    Update simple.fs

--- shaders/simple.fs
+++ shaders/simple.fs
@@ -1,8 +1,9 @@
 #version 330 core
 
 in vec3 outPos;
+out vec4 outColor;
 
 void main() {
   vec2 st = gl_FragCoord.xy / vec2(outPos.x, outPos.y);
-  gl_FragColor = vec4(st.x, st.y, 0.0f, 1.0f);
+  outColor = vec4(st.x, st.y, 0.0f, 1.0f);
 }
