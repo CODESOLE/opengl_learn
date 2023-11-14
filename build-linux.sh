a78042c915c4ac6d810581d715e4edfd6b704e29
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2023-11-14 14:28:22 +0300

    update build-linux.sh script

--- build-linux.sh
+++ build-linux.sh
@@ -4,12 +4,12 @@ if [ "$1" = "release" ]
 then
   conan profile detect --force
   conan install . --build=missing --settings=build_type=Release
-  cmake --preset conan-default
-  cmake --build build --config Release
+  cmake --preset conan-release
+  cmake --build build/Release --config Release
 else
   conan profile detect --force
   conan install . --build=missing --settings=build_type=Debug
-  cmake --preset conan-default
-  cmake --build build --config Debug
+  cmake --preset conan-debug
+  cmake --build build/Debug --config Debug
 fi
 
